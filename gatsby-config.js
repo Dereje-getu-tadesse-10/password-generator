5f2a949c5d1a54a9af29905025a4a29d2423399d
Dereje <getutadessedereje@icloud.com>
2022-12-08 20:44:03 +0100

    Fix: add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,14 +18,13 @@ module.exports = {
       {
           resolve: `gatsby-plugin-manifest`,
           options: {
-              name: `gatsby-starter-default`,
-              short_name: `starter`,
+              name: `Générateur de mot de passe`,
+              short_name: `GP`,
               start_url: `/`,
               background_color: `#166534`,
               theme_color: `#166534`,
               display: `minimal-ui`,
-              icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-
+              icon: `static/favicon.ico`
           }
       }
   ]
