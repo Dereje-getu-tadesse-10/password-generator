4da4a770e5db3697a5cbcf8cf21a2a27fbdc879b
Dereje <getutadessedereje@icloud.com>
2022-12-08 13:25:51 +0100

    Inital commit

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+export default function Error() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
+
